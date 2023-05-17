ed69dacf40e1a584809d8c7a2717daeb2c90330c
Norhanahmedsaber <norhan3182001@gmail.com>
2023-05-17 20:42:44 +0300

    hello world

--- app.js
+++ app.js
@@ -1,17 +1,58 @@
 const puppeteer = require('puppeteer')
+const axios = require('axios')
+const cheerio = require('cheerio')
+async function ram(search) {
+    const url = `https://ram-e-shop.com/?s=${search.replace(' ', '+')}&post_type=product`;
+    try {
 
-const anas = async (component) => {
-    const browser = await puppeteer.launch({headless:false});
-    const page = await browser.newPage();
-    await page.goto(`https://ram-e-shop.com/?s=${component.replace(" ", "+")}&product_cat=0&post_type=product`, {
-        waitUntil: "domcontentloaded"
-    })
-    const data =await page.evaluate(()=>{
-        const output = {}
-        output.name = Array.from(document.querySelectorAll("h2.woocommerce-loop-product__title")).map((element) => element.textContent)[0]
-        output.price=Array.from(document.querySelectorAll(".woocommerce-Price-amount.amount")).map((element)=> element.innerHTML)[0]
-        return output
-    })
-    console.log(data)
+      const { data } = await axios.get(url);
+      const $ = cheerio.load(data);
+
+      const prices = $("bdi");
+      const names  = $(".woocommerce-loop-product__title");
+      const images = $(".attachment-woocommerce_thumbnail.size-woocommerce_thumbnail");
+
+      const output = {
+        price: $(prices[1]).text(),
+        name : $(names[0]).text(),
+        image : $(images[0]).attr('src')
+      }
+      
+
+      return output
+    } catch (err) {
+      console.error(err);
+    }
 }
-anas("flex sensor");
+async function free(search) {
+    const url = `https://free-electronic.com/?s=${search.replace(' ', '+')}&product_cat=0&post_type=product`;
+    try {
+      const { data } = await axios.get(url);
+      const $ = cheerio.load(data);
+
+      const prices = $(".woocommerce-Price-amount.amount");
+      const names = $(".woocommerce-loop-product__title");
+      const images = $(".attachment-woocommerce_thumbnail.size-woocommerce_thumbnail");
+      
+      const output = {
+        price: $(prices[17]).text(),
+        name : $(names[16]).text(),
+        image :$(images[16]).attr('src')
+      }
+
+      
+      return output
+    } catch (err) {
+      console.error(err);
+    }
+}
+const search = async(search) => {
+    const data = {
+        ram: await ram(search),
+        free: await free(search),
+        // uge: uge(search),
+        // future: future(search)
+    }
+    return data
+}
+search("Arduino UNO").then((res) => console.log(res))
